b8656ae21ab494b49013a7cd67facff7bdbd7cf5
Thomas Ferreira <thomas_ferreira@yahoo.fr>
2017-10-10 22:23:19 +0200

    Add decode_responses option to redis

--- index.py
+++ index.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from flask_redis import FlaskRedis
+from redis import StrictRedis
 from config import BaseConfig
+
+
+class DecodedRedis(StrictRedis):
+    @classmethod
+    def from_url(cls, url, db=None, **kwargs):
+        kwargs['decode_responses'] = True
+        return StrictRedis.from_url(url, db, **kwargs)
+
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
-db = FlaskRedis(app)
+db = FlaskRedis.from_custom_provider(DecodedRedis, app)
+
